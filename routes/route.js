879bc7d6c718f5503c02f0980adbd188cf654b1a
danishansarib3 <41053422+danishansarib3@users.noreply.github.com>
2020-01-05 12:43:55 +0545

    first commit

---
+++ routes/route.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        data: {},
+        errors: {}
+    })
+})
+
+router.post('/contact ', (req, res) => {
+    res.render('contact', {
+        data: req.body; //{ message, email }
+        errors: {
+            message: {
+                msg: 'A message is required'
+            },
+            email: {
+                msg: "That email doesn't look right "
+            }
+        }
+    })
+})
+
+module.exports = router
